3d87c858e641c5fd861490cb00e79b6b77e07540
binbashz <5732@holbertonstudents.com>
2023-08-28 10:43:44 -0700

    floor ⌊x⌋

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+""" Basic annotations - floor """
+
+
+def floor(n: float) -> int:
+    """ returs floor of a float, take a float n as a argument
+     and returns tje floor of the float"""
+    return int(n)
