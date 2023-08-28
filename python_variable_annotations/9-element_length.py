c2596110f466c1220628c50665edb1660928fdf2
binbashz <5732@holbertonstudents.com>
2023-08-28 14:42:02 -0700

    list tuple

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Calculate the length of elements in a list
+"""
+from typing import List, Tuple
+
+
+def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+    """
+    Calculate the length of elements in a list.
+
+    Args:
+        lst (List[str]): The input list of strings.
+
+    Returns:
+        List[Tuple[str, int]]: A list of tuples
+        where each tuple contains a string and its length.
+    """
+    return [(i, len(i)) for i in lst]
