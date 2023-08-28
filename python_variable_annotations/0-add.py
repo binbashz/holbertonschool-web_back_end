4c34e467b62bdffe16488b31727de214f9d6d7bd
binbashz <5732@holbertonstudents.com>
2023-08-28 10:24:31 -0700

    Basic add annotaions

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Basic annotations - add """
+
+
+def add(a: float, b: float) -> float:
+
+    """
+    Add two floating-point numbers.
+
+    Args:
+        a (float): The first floating-point number.
+        b (float): The second floating-point number.
+
+    Returns:
+        float: The sum of 'a' and 'b'.
+    """
+    # Arg annotations (a: float, b: float) indicate 'a' & 'b' are float values.
+    # Return annotation (-> float) indicates function returns a float value.
+
+    # Sum the input numbers and return the result
+    return a + b
