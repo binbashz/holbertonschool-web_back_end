e032cb0fb118432827fc5abbfe1a4b9f7aa2c3d9
binbashz <5732@holbertonstudents.com>
2023-08-28 10:48:24 -0700

    Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+""" Basic annotations - floor """
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        str: The string representation of 'n'.
+    """
+    return str(n)
