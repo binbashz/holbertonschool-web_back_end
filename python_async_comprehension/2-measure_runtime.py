27fd5423222a6cc7f3a3d9b9ca2312b6b0dcd22a
binbashz <5732@holbertonstudents.com>
2023-09-01 08:02:41 -0700

    asyncio.gather

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Run time for four parallel comprehensions
+"""
+import asyncio
+import time
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measures the runtime of async_comprehension()
+    """
+    init_time = time.time()
+    # waits for all the async operations to complete
+    await asyncio.gather(async_comprehension())
+    end_time = time.time()
+    return end_time - init_time
