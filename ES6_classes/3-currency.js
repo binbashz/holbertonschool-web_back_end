4fd02b0267f2feb5ebf3579b9a9304273c91bea8
binbashz <5732@holbertonstudents.com>
2023-09-16 14:57:45 -0700

    ´ESlindt´

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,30 +1,30 @@
 export default class Currency {
-	constructor(code, name) {
-	if (typeof code !== 'string') throw TypeError('Code must be  string');
-	if (typeof name !== 'string') throw TypeError('Name must be  string');
-	this._code = code;
-	this._name = name;
-	}
-  
-	get code() {
-	return this._code;
-	}
-  
-	set code(nuevoCode) {
-	if (typeof nuevoCode !== 'string') throw TypeError('Code must be a string');
-	this._code = nuevoCode;
-	}
-  
-	get name() {
-	return this._name;
-	}
-  
-	set name(nuevoName) {
-	if (typeof nuevoName !== 'string') throw TypeError('Name must be a string');
-	this._name = nuevoName;
-	}
-  
-	displayFullCurrency() {
-	return `${this.name} (${this.code})`;
-	}
-		}
+  constructor(code, name) {
+    if (typeof code !== 'string') throw TypeError('Code must be string');
+    if (typeof name !== 'string') throw TypeError('Name must be string');
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(nuevoCode) {
+    if (typeof nuevoCode !== 'string') throw TypeError('Code must be string');
+    this._code = nuevoCode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(nuevoName) {
+    if (typeof nuevoName !== 'string') throw TypeError('Name must be string');
+    this._name = nuevoName;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
