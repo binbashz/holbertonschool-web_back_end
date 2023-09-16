6455692c3d862803cfa04c753fdb41157d827735
binbashz <5732@holbertonstudents.com>
2023-09-16 16:37:53 -0700

    objetos HolbertonClass que pueden ser tratados como números o cadenas de texto según sea necesario

---
+++ ES6_classes/8-main.js
@@ -0,0 +1,9 @@
+import HolbertonClass from "./8-hbtn_class.js";
+
+const hc = new HolbertonClass(12, "Mezzanine")
+console.log(Number(hc));
+console.log(String(hc));
+
+bob@dylan:~$ 
+bob@dylan:~$ npm run dev 8-main.js 
+12
