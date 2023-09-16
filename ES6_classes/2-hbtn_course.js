26a67b2d4176d1f909e065ae8b958f48d43600b5
binbashz <5732@holbertonstudents.com>
2023-09-16 14:26:55 -0700

    class constructor

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,37 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') throw TypeError('Name must be a string');
+    if (typeof length !== 'number') throw TypeError('Length must be a number');
+    if (!Array.isArray(students)) throw TypeError('Students must be an array');
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(names) {
+    if (typeof names !== 'string') throw TypeError('Name must be a string');
+    this._name = names;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(len) {
+    if (typeof len !== 'number') throw TypeError('Length must be a number');
+    this._length = len;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(student) {
+    if (!Array.isArray(student)) throw TypeError('Students must be an array');
+    this._students = student;
+  }
+}
