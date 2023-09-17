d7b32f0afa6fe187d46044575a05593bbe68deb4
binbashz <5732@holbertonstudents.com>
2023-09-17 10:20:42 -0700

    class and method

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,11 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    // create a new object with the constructor of current instance
+    return new this.constructor();
+  }
