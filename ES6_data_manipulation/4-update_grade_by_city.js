df9a47d408560db70ac8f7a30cf1e82fcf598569
binbashz <5732@holbertonstudents.com>
2023-10-09 19:11:14 +0000

    fix spaces

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,13 +3,12 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-// Find a matching grade object for the student by studentId
+      // Find a matching grade object for the student by studentId
       const newGrade = newGrades.find((grade) => grade.studentId === student.id);
-// Create a new student object with updated grade
+      // Create a new student object with updated grade
       return {
-        ...student,// Spread the existing student properties
-        grade: newGrade ? newGrade.grade : 'N/A',// Update the grade based on newGrades or set to 'N/A' if not found
+        ...student, // Spread the existing student properties
+        grade: newGrade ? newGrade.grade : 'N/A', // Update the grade based on newGrades or set to 'N/A' if not found
       };
     });
 }
-
