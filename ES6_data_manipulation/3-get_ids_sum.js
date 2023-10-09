5a885a147fca2d6e74c9a311232cdffbe51a2f54
binbashz <5732@holbertonstudents.com>
2023-10-09 18:48:47 +0000

    fix

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,4 @@
-// import the getListStudents function to get the list of students
-// Define the getStudentIdsSum function that calculates the sum of student IDs
-exprot default function getStudentIdsSum(students) {
+export default function getStudentIdsSum(students) {
   // Use the reduce method to iterate through the array of students and accumulate the sum
   const total = students.reduce((sumar, student) => sumar + student.id, 0);
   // Return the final accumulated sum
