a23cc3218fdbb7ecd4daf6c170d8f1d0d49509e9
binbashz <5732@holbertonstudents.com>
2023-10-09 19:08:27 +0000

    spcs

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,14 +1,14 @@
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  // Use filter to select students belonging to the specified city
+// Use filter to select students belonging to the specified city
   return students
     .filter((student) => student.location === city)
     .map((student) => {
-      // Find a matching grade object for the student by studentId
+// Find a matching grade object for the student by studentId
       const newGrade = newGrades.find((grade) => grade.studentId === student.id);
-      // Create a new student object with updated grade
+// Create a new student object with updated grade
       return {
-        ...student,             // Spread the existing student properties
-        grade: newGrade ? newGrade.grade : 'N/A', // Update the grade based on newGrades or set to 'N/A' if not found
+        ...student,// Spread the existing student properties
+        grade: newGrade ? newGrade.grade : 'N/A',// Update the grade based on newGrades or set to 'N/A' if not found
       };
     });
 }
