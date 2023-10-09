bbaf96d4dd54dc1384534e1ea138bb9aabc2fea9
binbashz <5732@holbertonstudents.com>
2023-10-09 19:13:15 +0000

    expect ident space

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 export default function getStudentsByLocation(students, city) {
 // filtre the studen by city
-   return students.filter((student) => student.location === city);
+  return students.filter((student) => student.location === city);
 }
