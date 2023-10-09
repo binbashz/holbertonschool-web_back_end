ebf9837a6eaf5dac8664790f8111c8475c538e58
binbashz <5732@holbertonstudents.com>
2023-10-09 17:43:37 +0000

    extracts and returns the values of the id property from a list of objects, or returns an empty array

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,14 @@
+function getListStudentIds(studentList) {
+  // Verifica si studentList es un array
+  if (!Array.isArray(studentList)) {
+    return [];
+  }
+
+  // Usa map() para extraer los IDs y crear un nuevo array
+  const idList = studentList.map((student) => student.id);
+
+  return idList;
+}
+
+export default getListStudentIds;
+
