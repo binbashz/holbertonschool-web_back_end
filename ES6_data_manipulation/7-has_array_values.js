ccc5295f1810572e08479a57b8eaaa2979cd50f7
binbashz <5732@holbertonstudents.com>
2023-10-09 19:44:05 +0000

    fix param

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,4 @@
-function hasValuesFromArray(set, arr) {
+export default function hasValuesFromArray(set, array) {
   // Use the every() method to check if all elements in the array are in the set
-  return arr.every((data) => set.has(data));
+  return array.every((data) => set.has(data));
 }
