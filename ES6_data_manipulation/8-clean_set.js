253bc9c70f641f71bb624f0c055814e5ce08d420
binbashz <5732@holbertonstudents.com>
2023-10-09 19:56:24 +0000

    fix returns expected string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,7 @@
 export default function cleanSet(set, startString) {
   // Use the filter() method to select values that start with startString
   const selectedValues = [...set].filter((value) => value.startsWith(startString));
-
   // Join the selected values with "-"
-  const resultString = selectedValues.join('-');
-
-  return resultString;
+  return selectedValues.join('-');
 }
 
